a3c6d3c0e3dc35404228086f6fa28b46e11a552f
shawtygotsomeskills <aalubhujiya29@gmail.com>
2026-08-22 23:52:41 +0530

    Add files via upload

---
+++ python list.py
@@ -0,0 +1,8 @@
+import numpy as np
+import random
+my_list = [1, "puja", 34, 67, 78]
+print(my_list)
+ar = np.array([1, 2, 3, 4, 5])
+print(ar)
+a = np.random.randint(1, 10, size=(3, 4))
+print(a)  
