43f3e7b7849357ac5b8b0a7000b36b234d0d91a7
shawtygotsomeskills <aalubhujiya29@gmail.com>
2026-08-22 23:27:46 +0530

    Add files via upload

---
+++ string manipulation program.py
@@ -0,0 +1,9 @@
+text="hi"
+upper_text = text.upper()
+lower_text = text.lower()
+split_text = text.split()
+
+print("original text:" , text)
+print("split text:" , split_text)
+print("length:", len(text))
+print("first word:" , text [:6])
